6e34a38ccc2bcd5dd300e31fdf4ccc206a2d9c10
Farhin Mashiat Mayabee <38862223+mashiatmayabee@users.noreply.github.com>
2022-01-01 10:25:11 +0600

    More OS practice codes

--- Thread/threadCreation.c
+++ Thread/threadCreation.c
@@ -2,19 +2,15 @@
 #include<pthread.h>
 int sum(){
 	int i;
-	for(i=0;i<50;i++){
 		printf("sum function\n");
-	}
 	
 }
-
 int main(){
 	int i;
 	pthread_t threadID;
 	pthread_create(&threadID,NULL, (void*)sum,NULL );
-	sum();
-	for(i=0;i<50;i++){
+	
 		printf("Main function\n");
-	}
+	
 	return 0;
 }
